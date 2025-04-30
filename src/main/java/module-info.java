210ce4205ebe5fef38efdb0df27fa69f59b7ef9d
AlbertoKimi <breakingdawn1988@gmail.com>
2025-04-30 18:48:51 +0200

    module cambio

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,9 @@ module com.enriquealberto {
     opens com.enriquealberto to javafx.fxml;
     opens com.enriquealberto.Controladores to javafx.fxml;
     requires javafx.media;
+    exports com.enriquealberto; // Exporta el paquete principal
+    exports com.enriquealberto.Controladores; // Exporta los controladores si los usas en FXML
+    exports com.enriquealberto.model;
 
     
 }
