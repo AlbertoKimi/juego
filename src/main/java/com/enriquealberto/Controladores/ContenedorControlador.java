44e2cea5076073e096929fe95bb81c6803ddfae6
enriquesda <enriquealbuera60@gmail.com>
2025-04-30 20:22:03 +0200

    arreglo

--- src/main/java/com/enriquealberto/Controladores/ContenedorControlador.java
+++ src/main/java/com/enriquealberto/Controladores/ContenedorControlador.java
@@ -1,37 +1,50 @@
 package com.enriquealberto.Controladores;
 
-import com.enriquealberto.EscenaID;
-import com.enriquealberto.ManagerEscenas;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
 
 public class ContenedorControlador {
 
     @FXML
-    AnchorPane juego;
+    private AnchorPane juego;
 
     @FXML
-    AnchorPane estadistica;
+    private AnchorPane estadistica;
 
     @FXML
     public void initialize() {
-        ManagerEscenas sm = ManagerEscenas.getInstance();
+        try {
+            // Cargar Juego.fxml directamente (sin usar ManagerEscenas)
+            FXMLLoader juegoLoader = new FXMLLoader(getClass().getResource("/com/enriquealberto/vistas/Juego.fxml"));
+            Parent juegoRoot = juegoLoader.load();
+            
+            // Ajustar anclas para que ocupe todo el espacio
+            AnchorPane.setTopAnchor(juegoRoot, 0.0);
+            AnchorPane.setBottomAnchor(juegoRoot, 0.0);
+            AnchorPane.setLeftAnchor(juegoRoot, 0.0);
+            AnchorPane.setRightAnchor(juegoRoot, 0.0);
+            
+            juego.getChildren().setAll(juegoRoot); // Añadir al panel izquierdo
 
-        Scene addjuego = sm.getScene(EscenaID.JUEGO);
-        AnchorPane.setBottomAnchor(addjuego.getRoot(), 0.0);
-        AnchorPane.setTopAnchor(addjuego.getRoot(), 0.0);
-        AnchorPane.setLeftAnchor(addjuego.getRoot(), 0.0);
-        AnchorPane.setRightAnchor(addjuego.getRoot(), 0.0);
-        juego.getChildren().setAll(addjuego.getRoot());
+            // Cargar Estadisticas.fxml directamente
+            FXMLLoader estLoader = new FXMLLoader(getClass().getResource("/com/enriquealberto/vistas/estadisticas.fxml"));
+            Parent estRoot = estLoader.load();
+            
+            // Ajustar anclas para que ocupe todo el espacio
+            AnchorPane.setTopAnchor(estRoot, 0.0);
+            AnchorPane.setBottomAnchor(estRoot, 0.0);
+            AnchorPane.setLeftAnchor(estRoot, 0.0);
+            AnchorPane.setRightAnchor(estRoot, 0.0);
+            
+            estadistica.getChildren().setAll(estRoot); // Añadir al panel derecho
 
-        Scene addest = sm.getScene(EscenaID.ESTADISTICAS);
-        AnchorPane.setBottomAnchor(addest.getRoot(), 0.0);
-        AnchorPane.setTopAnchor(addest.getRoot(), 0.0);
-        AnchorPane.setLeftAnchor(addest.getRoot(), 0.0);
-        AnchorPane.setRightAnchor(addest.getRoot(), 0.0);
-        estadistica.getChildren().setAll(addest.getRoot());
-
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error al cargar los paneles internos del contenedor.");
+        }
     }
 }
